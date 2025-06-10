1cde64870ed5c8a58357b3f6a6bdff191cffe674
ayesharizwannn <rizwanayesha588@gmail.com>
2025-06-11 01:21:52 +0500

    added chooseWord,checkWin, printWord , checkWin functions

--- hangman game.cpp
+++ hangman game.cpp
@@ -39,44 +39,68 @@ void clearScreen(){
 }
 
 
-void printHeader(char *name , int score , int level){
-	printf("=============================================\n\n");
-	printf("player: %s\tScore:%d\tLevel:%s\n " name , score ,(level==1 ? "EASY" :level ==2? "MODERATE": "HARD");
-	printf("=============================================\n\n");
+void printHeader(char *name, int score, int level) {
+    printf("=============================================\n");
+    printf("  Player: %s\tScore: %d\tLevel: %s\n", name, score,
+           (level == 1 ? "Easy" : level == 2 ? "Moderate" : "Hard"));
+    printf("=============================================\n\n");
 }
 
-void printHangman(int tries){
-	switch (tries){
-		case 1:printf("\O\n"); break;
-		case 2: printf("\n O\n | \n") ; break ;
-		case 3: printf("\n O\n |/ \n ") ; break ;
-		case 4: printf("\n O\n /|\ \n ") ; break ;
-		case 5: printf("\n O\n/|\\\n/\n"); break;
-		case 6: printf("\n O\n/|\\\n/ \\\n"); break;
-		case 7: printf("\n O\n/|\\\n/ \\\n\nGame Over!\n"); break;
-	}
+void printHangman(int tries) {
+    switch (tries) {
+        case 1: printf("\n O\n"); break;
+        case 2: printf("\n O\n |\n"); break;
+        case 3: printf("\n O\n |/\n"); break;
+        case 4: printf("\n O\n/|\\\n"); break;
+        case 5: printf("\n O\n/|\\\n/\n"); break;
+        case 6: printf("\n O\n/|\\\n/ \\\n"); break;
+        case 7: printf("\n O\n/|\\\n/ \\\n\nGame Over!\n"); break;
+        default: break;
+    }
 }
 
+void printWord(char *word, int *guessed) {
+    for (int i = 0; i < strlen(word); i++) {
+        if (guessed[i]) {
+            printf("%c ", word[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
 
+int checkWin(int *guessed, int wordLength) {
+    for (int i = 0; i < wordLength; i++) {
+        if (!guessed[i]) return 0;
+    }
+    return 1;
+}
 
+void chooseWord(int level, char *word, char *hint) {
+    int index = rand() % WORD_LIST_SIZE;
+    if (level == 1) {
+        strcpy(word, easyWords[index]);
+        strcpy(hint, easyHints[index]);
+    } else if (level == 2) {
+        strcpy(word, moderateWords[index]);
+        strcpy(hint, moderateHints[index]);
+    } else {
+        strcpy(word, hardWords[index]);
+        strcpy(hint, hardHints[index]);
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void chooseLevel(int *level, int *maxTries) {
+    printf("Select Level:\n1. Easy\n2. Moderate\n3. Hard\nEnter choice: ");
+    scanf("%d", level);
+    switch (*level) {
+        case 1: *maxTries = MAX_TRIES_EASY; break;
+        case 2: *maxTries = MAX_TRIES_MODERATE; break;
+        case 3: *maxTries = MAX_TRIES_HARD; break;
+        default: *level = 1; *maxTries = MAX_TRIES_EASY;
+    }
+}
 
 
 
